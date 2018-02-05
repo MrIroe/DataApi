59a1d16e7521158cac837f7ba2b59a76d5e4b616
Nadir Saleh <Nadir@Nadirs-Mac.local>
2018-02-04 19:42:06 -0500

    added console log for port listening

--- web.go
+++ web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,6 +13,7 @@ import (
 
 func StartWeb() {
 	router := NewRouter()
+	fmt.Printf("Now listening on port: %v", util.GetConfigValue("dataApiPort"))
 	log.Fatal(http.ListenAndServe(util.GetConfigValue("dataApiPort"), router))
 }
 

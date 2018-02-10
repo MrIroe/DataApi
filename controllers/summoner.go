f10808344bb2b4e0ed27618c37856d0032734764
Nadir Saleh <Nadir@Nadirs-Mac.local>
2018-02-10 02:21:47 -0500

    got summonerInfo by accountId working

--- controllers/summoner.go
+++ controllers/summoner.go
@@ -4,6 +4,7 @@ import (
 	mongo "dataApi/mongo"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -11,6 +12,7 @@ import (
 func RegisterSummonerHandlers(router *mux.Router, route string) {
 	summoner := router.PathPrefix(route).Subrouter()
 	summoner.HandleFunc("/byName/{summonerName}", GetSummonerInfo)
+	summoner.HandleFunc("/byAccId/{accountId}", GetSummonerInfoAcc)
 }
 
 func GetSummonerInfo(w http.ResponseWriter, req *http.Request) {
@@ -21,6 +23,28 @@ func GetSummonerInfo(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		errLog := fmt.Sprintf("Error in GetSummonerInfo: %v", err)
 		WriteResponse(w, 400, false, errLog, nil)
+		return
+	}
+
+	WriteResponse(w, 200, true, "", summonerInfo)
+}
+
+func GetSummonerInfoAcc(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	accountId := vars["accountId"]
+
+	intAccountId, err := strconv.ParseInt(accountId, 10, 64)
+	if err != nil {
+		errLog := fmt.Sprintf("Error parsing string: %v", err)
+		WriteResponse(w, 400, false, errLog, nil)
+		return
+	}
+
+	summonerInfo, err := mongo.GetSummonerInfoAcc(intAccountId)
+	if err != nil {
+		errLog := fmt.Sprintf("Error in GetSummonerInfo: %v", err)
+		WriteResponse(w, 400, false, errLog, nil)
+		return
 	}
 
 	WriteResponse(w, 200, true, "", summonerInfo)
--- mongo/mongoRepo.go
+++ mongo/mongoRepo.go
@@ -48,7 +48,7 @@ func UpdateSummonerStats(summonerStats *obj.SummonerMatchStats) error {
 	defer localSess.Close()
 	c := localSess.DB(entityDbName).C(summonerStatsColName)
 
-	_, err := c.Upsert(bson.M{"AccountId": summonerStats.AccountId, "$and": bson.M{"MatchId": summonerStats.MatchId}}, &summonerStats)
+	_, err := c.Upsert(bson.M{"$and": []bson.M{bson.M{"AccountId": summonerStats.AccountId}, bson.M{"MatchId": summonerStats.MatchId}}}, &summonerStats)
 	if err != nil {
 		return errors.Wrap(err, "Error updating summoner stats")
 	}
@@ -61,7 +61,7 @@ func UpdateMatchReference(matchReference *obj.MatchReferenceData) error {
 	defer localSess.Close()
 	c := localSess.DB(entityDbName).C(matchRefColName)
 
-	_, err := c.Upsert(bson.M{"AccountId": matchReference.AccountId, "$and": bson.M{"MatchId": matchReference.MatchId}}, &matchReference)
+	_, err := c.Upsert(bson.M{"$and": []bson.M{bson.M{"AccountId": matchReference.AccountId}, bson.M{"MatchId": matchReference.MatchId}}}, &matchReference)
 	if err != nil {
 		return errors.Wrap(err, "Error updating match reference")
 	}
@@ -81,4 +81,18 @@ func GetSummonerInfo(summonerName string) (*obj.SummonerDTO, error) {
 	}
 
 	return &summonerInfo, nil
+}
+
+func GetSummonerInfoAcc(accountId int64) ([]obj.SummonerDTO, error) {
+	localSess := *primarySess.Clone()
+	defer localSess.Close()
+	c := localSess.DB(entityDbName).C(summonerColName)
+
+	var summonerInfo []obj.SummonerDTO
+	err := c.Find(bson.M{"AccountId": accountId}).All(&summonerInfo)
+	if err != nil {
+		return nil, errors.Wrap(err, "Error getting SummonerInfo")
+	}
+
+	return summonerInfo, nil
 }

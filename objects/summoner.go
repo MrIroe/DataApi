264e23ddd0de1caa3aea26ea3ea28424cc50ce0c
Nadir Saleh <Nadir@Nadirs-Mac.local>
2018-04-02 08:29:59 -0400

    added queueId and seasonId to matchDetail struct

--- objects/summoner.go
+++ objects/summoner.go
@@ -25,6 +25,8 @@ type SummonerDTO struct {
 
 type SummonerMatchStats struct {
 	AccountId  int64                  `json:"accountId" bson:"AccountId"`
+	SeasonId   int                    `json:"seasonId" bson:"SeasonId"`
+	QueueId    int                    `json:"queueId" bson:"QueueId"`
 	MatchId    int64                  `json:"matchId" bson:"MatchId"`
 	ChampionId int                    `json:"championId" bson:"ChampionId"`
 	Stats      ParticipantStatsDto    `json:"stats" bson:"Stats"`
